c06c8c4d73a14a05975d3be99a80dc2c108c802e
Mikhail Prokhorov <mprohorov@evapps.ru>
2022-06-05 16:14:54 +0300

    api mongo mongo-express

---
+++ api/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+const app = express();
+
+const port = 8080;
+const path = __dirname + '/views/';
+
+router.use(function timeLog(req, res, next) {
+    console.log(`Time: ${Date.now()}`);
+    next();
+})
+
+router.get('/', (req, res) => {
+    res.sendFile(path + 'index.html');
+})
+
+router.post('/', (req, res) => {
+    res.send('Got a POST request');
+});
+
+router.get('/sharks', function(req, res) {
+    res.sendFile(path + 'sharks.html');
+});
+
+app.use(express.static(path))
+app.use('/', router)
+
+app.listen(port, () => {
+    console.log(path);
+    console.log(`Example app listening on port ${port}`);
+});
---
+++ api/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const MONGO_USERNAME = 'pro6ka';
+const MONGO_PASSWORK = 'pro6ka/password';
+const MONGO_HOSTNAME = '127.0.0.1';
+const MONGO_PORT = '27017'
+const MONGO_DB = 'sharkinfo';
