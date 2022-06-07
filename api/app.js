360a4b8eafc4e2f320047f550c6e75575b9f322b
Mikhail Prokhorov <mprohorov@evapps.ru>
2022-06-07 10:48:41 +0300

    connect to database

--- api/app.js
+++ api/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./db');
-console.log(db);
+// const db = require('./db');
+const mongoose = require('mongoose');
+const MONGO_USERNAME = 'mongoadmin';
+const MONGO_PASSWORD = 'mongoadmin';
+const MONGO_HOSTNAME = 'mongodb';
+const MONGO_PORT = '27017'
+const MONGO_DB = 'sharkinfo';
+const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Connected to the database!');
+}).catch(err => {
+    console.error(err);
+    process.exit();
+});
 
 const app = express();
 
@@ -9,6 +24,7 @@ const port = 8080;
 const path = __dirname + '/views/';
 
 router.use(function timeLog(req, res, next) {
+    console.log(url);
     console.log(`Time: ${Date.now()}`);
     next();
 })
@@ -29,6 +45,5 @@ app.use(express.static(path))
 app.use('/', router)
 
 app.listen(port, () => {
-    console.log(path);
     console.log(`Example app listening on port ${port}`);
 });
