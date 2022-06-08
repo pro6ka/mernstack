eced8c2f5ef86dd4bba154e232d5c8dfc7217f70
Mikhail Prokhorov <mprohorov@evapps.ru>
2022-06-08 11:19:25 +0300

    no hardcode form db config

--- api/db.js
+++ api/db.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
 
+/*
 const MONGO_USERNAME = 'mongoadmin';
 const MONGO_PASSWORD = 'mongoadmin';
 const MONGO_HOSTNAME = 'mongodb';
 const MONGO_PORT = '27017'
 const MONGO_DB = 'sharkinfo';
+*/
+const {
+    MONGO_USERNAME,
+    MONGO_PASSWORD,
+    MONGO_HOSTNAME,
+    MONGO_PORT,
+    MONGO_DB,
+} = process.env;
+
 const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 mongoose.connect(url, {
